2597fffcb4e29a0496ae2ccf7bbd064ff45c2f0a
grandnumber & Katrina Eaton <pair+grandnumber.k-eaton@devbootcamp.com>
2015-03-19 16:56:17 -0700

    update seed file with follow relationships

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
 require 'faker'
 
 5.times do
- user = User.create(name: Faker::Name.name, email: Faker::Internet.email, handle: Faker::Name.first_name.downcase)
+  user = User.create(name: Faker::Name.name, email: Faker::Internet.email, handle: Faker::Name.first_name.downcase)
 
- 5.times {user.tweets.create(content: Faker::Lorem.sentence)}
+  5.times {user.tweets.create(content: Faker::Lorem.sentence)}
 end
+
+Follow.create(user_id: 1, subscribed_to_id: 2)
+Follow.create(user_id: 1, subscribed_to_id: 3)
+Follow.create(user_id: 1, subscribed_to_id: 4)
+Follow.create(user_id: 1, subscribed_to_id: 5)
+Follow.create(user_id: 2, subscribed_to_id: 5)
+Follow.create(user_id: 3, subscribed_to_id: 5)
+Follow.create(user_id: 4, subscribed_to_id: 5)
+Follow.create(user_id: 3, subscribed_to_id: 4)
+
 
 

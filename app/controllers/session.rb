fbcbbd029f2da83fa0ed658c6a5b68498146d708
grandnumber & Katrina Eaton <pair+grandnumber.k-eaton@devbootcamp.com>
2015-03-19 12:00:46 -0700

    working sign in and login w validations

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,4 +1,5 @@
 get '/users/new' do
+  @user=User.new
   erb :signup
 end
 
@@ -8,6 +9,22 @@ post '/users' do
     session[:id] = @user.id
     redirect "/"
   else
-    # Error Handling
+    erb :signup
+  end
+end
+
+get '/sessions/new' do
+  @user=User.new
+  erb :signin
+end
+
+post '/sessions' do
+  @user = User.where(handle: params[:handle])[0]
+  if @user && @user.authenticate(params[:password])
+    session[:id] = @user.id
+    redirect "/"
+  else
+    @errors="your login credentials failed"
+    erb :signin
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
   include BCrypt
-
+validates :email, :handle, presence:true
+validate :orrect_email_format
 # getter method:
   def password
     @password ||= Password.new(password_hash)
@@ -10,6 +11,14 @@ class User < ActiveRecord::Base
   def password=(new_password)
     @password = Password.create(new_password)
     self.password_hash = @password
+  end
+
+  def authenticate(input_password)
+    self.password == input_password
+  end
+
+  def correct_email_format
+    self.errors.add :base, "Incorrect email address format" unless /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i.match(self.email)
   end
 
 end

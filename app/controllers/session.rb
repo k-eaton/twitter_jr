4d0d6503d07f22743c27252a7b4210c67eeec2b7
grandnumber & Katrina Eaton <pair+grandnumber.k-eaton@devbootcamp.com>
2015-03-19 11:35:26 -0700

    successfully saving users

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -2,4 +2,12 @@ get '/users/new' do
   erb :signup
 end
 
-
+post '/users' do
+  @user = User.new(params[:user])
+  if @user.save
+    session[:id] = @user.id
+    redirect "/"
+  else
+    # Error Handling
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
 class User < ActiveRecord::Base
-  # Remember to create a migration!
+  include BCrypt
+
+# getter method:
+  def password
+    @password ||= Password.new(password_hash)
+  end
+
+# setter method:
+  def password=(new_password)
+    @password = Password.create(new_password)
+    self.password_hash = @password
+  end
+
 end

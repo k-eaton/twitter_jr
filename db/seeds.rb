271942ef77709dde4f21fdad99c918c4030eacb8
tonymai <thetonymai@gmail.com>
2015-03-19 11:14:14 -0700

    Create tweet model and seed database

---
+++ db/seeds.rb
@@ -0,0 +1,9 @@
+require 'faker'
+
+5.times do
+ user = User.create(name: Faker::Name.name, email: Faker::Internet.email, handle: Faker::Name.first_name)
+
+ 5.times {user.tweets.create(content: Faker::Lorem.sentence)}
+end
+
+

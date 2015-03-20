ec11793fa230f10ca90d9e3e4dc9e70a0395ffca
grandnumber & Katrina Eaton <pair+grandnumber.k-eaton@devbootcamp.com>
2015-03-19 18:09:35 -0700

    Working add follower, waiting only follow once condition

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -1,7 +1,7 @@
 #rename to users.rb
 
 get '/users/new' do
-@user=User.new
+  @user=User.new
   erb :signup
 end
 
@@ -28,4 +28,15 @@ end
 get '/users/:id/followers' do
   @user=User.where(id:params[:id])[0]
   erb :show_followers
+end
+
+post '/users/:id' do
+  @user=User.where(id: params[:id])[0]
+  if @user.id == current_user.id || #if already following
+    "YOU CAN'T FOLLOW YOURSELF"
+    # redirect "/users/#{@user.id}"
+  else
+    Follow.create(user_id: current_user.id, subscribed_to_id: @user.id)
+    redirect "/users/#{@user.id}"
+  end
 end

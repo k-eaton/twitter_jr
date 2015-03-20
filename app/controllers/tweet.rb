83aedb1dc0b566f993bc3f5175f95d628653762f
Tony Mai <thetonymai@gmail.com>
2015-03-19 21:57:35 -0700

    refactored tweet and add retweet feature

--- app/controllers/tweet.rb
+++ app/controllers/tweet.rb
@@ -1,8 +1,8 @@
-# get '/tweet' do
-#   erb :tweet
-# end
+get '/tweets/new' do
+  erb :'tweet/new'
+end
 
-post '/tweet' do
+post '/tweets/new' do
   tweet = Tweet.new(content: params[:content], user_id: current_user.id)
   if tweet.save
     redirect "/users/#{current_user.id}"
@@ -11,6 +11,17 @@ post '/tweet' do
     @user = User.find(current_user.id)
     @content = params[:content]
     @error = tweet.errors.full_messages.to_sentence
-    erb :profile_page #confusing, need to figure out a better place
+    erb :'tweet/new'
   end
+end
+
+get '/tweets/:id' do
+  @tweet = Tweet.find(params[:id])
+  erb :'tweet/show'
+end
+
+get '/tweets/:id/retweet' do
+  tweet = Tweet.find(params[:id])
+  @content = "#{tweet.content} @#{tweet.user.handle}"
+  erb :'tweet/new'
 end

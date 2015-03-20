d48dc8bd652dc877eae9b30bbc23c5a7542e1655
Derek Tang & tonymai <pair+derekztang.tonymai@devbootcamp.com>
2015-03-20 12:58:01 -0700

    Favorite model and migration added

--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -1,6 +1,9 @@
 class Tweet < ActiveRecord::Base
   belongs_to :user
 
+  has_many   :favorites
+  has_many   :favorite_users, :through => :favorites
+
   validates :content, presence: true
   validates :content, length: { maximum: 138 }
 end

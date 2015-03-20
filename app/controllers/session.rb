bf70d33e3cd3df657f134af4f885abc5fd66cdb3
tonymai <thetonymai@gmail.com>
2015-03-19 18:35:37 -0700

    feed mvp

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,14 +1,14 @@
-get '/' do
-  if current_user
-   erb :test
-  else
-    redirect "/users/new"
-  end
-end
+# get '/' do
+#   if current_user
+#    erb :test
+#   else
+#     redirect "/users/new"
+#   end
+# end
 
 get '/sessions/new' do
   @user=User.new
-  erb :signin
+  erb :_signin
 end
 
 post '/sessions' do
@@ -18,12 +18,12 @@ post '/sessions' do
     redirect "/"
   else
     @errors="your login credentials failed"
-    erb :signin
+    erb :_signin
   end
 end
 
 
 delete '/sessions/logout' do
   session[:id] = nil
-  redirect '/sessions/new'
+  redirect '/'
 end

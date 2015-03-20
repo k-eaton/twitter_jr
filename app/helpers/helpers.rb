b1faa344aed80072f3fc9d7f96efa4aa17707e3c
Derek Tang & tonymai <pair+derekztang.tonymai@devbootcamp.com>
2015-03-20 13:15:50 -0700

    move show tweet helper to partial

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -7,11 +7,11 @@ helpers do
     !!current_user
   end
 
-  def show_tweet(tweet)
-    "Date: #{tweet.created_at.strftime('%b %d')}<br />
-     By: @#{tweet.user.handle}<br />
-     #{tweet.content}<br />
-     <a href='/tweets/#{tweet.id}/reply'>Reply</a> <a href='/tweets/#{tweet.id}/retweet'>Retweet</a>"
-  end
+  # def show_tweet(tweet)
+  #   "Date: #{tweet.created_at.strftime('%b %d')}<br />
+  #    By: @#{tweet.user.handle}<br />
+  #    #{tweet.content}<br />
+  #    <a href='/tweets/#{tweet.id}/reply'>Reply</a> <a href='/tweets/#{tweet.id}/retweet'>Retweet</a>"
+  # end
 
 end
